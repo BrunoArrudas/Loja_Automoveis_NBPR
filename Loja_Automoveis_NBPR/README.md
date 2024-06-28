6d74af38297b004ecc06267f4f98789946f635ac
BrunoArrudas <brunoguilhermechaveiro@gmail.com>
2024-06-27 21:19:16 -0300

    Atualizações Front-End e sistema Funcional 0.2

--- Loja_Automoveis_NBPR/README.md
+++
@@ -1,2 +0,0 @@
-# Loja_Automoveis_NBPR
-Repositorio criado com o objetivo de armazenar os codigos da Loja de Automoveis NBPR.
---
+++ contato.php
@@ -0,0 +1,37 @@
+<?php
+include_once 'header.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contato</title>
+    
+</head>
+<body>
+
+    <main>
+        <section class="about">
+            <h1>Fale Conosco</h1>
+            <p>Ainda precisa de ajuda? Entre em contato por um de nossos canais
+                <li>Fique atento! O prazo para retorno dos chamados é até 7 dias úteis a partir da abertura do chamado.</li></p> 
+               
+                <h2>Central de atendimento</h2>
+                <li>Horário de funcionamento: de Segunda a Sexta das 8h às 19h 
+                    e Sábado das 8h ás 13h</li>
+                    <br>
+
+                <h2>Central de vendas</h2>
+                <li>Horário de funcionamento: de Segunda a Sexta das 8h às 19h 
+                    e Sábado das 8h ás 13h</li>
+
+        </section> 
+    </main>
+</body>
+</html>
+
+<?php
+?>
