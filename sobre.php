1d7d231b1193671f06058a5aeefe4c6bceafd876
RaphaMitter <raphamitter27@gmail.com>
2024-07-03 21:59:38 -0300

    Front-End - Ajustes nas paginas, correção footer e ajustes nas imagens.

--- sobre.php
+++ sobre.php
@@ -17,6 +17,36 @@ include_once 'header.php';
             </ul>
             <h2>Junte-se a Nós:</h2>
             <p>Venha fazer parte da empresa "Automoveis_NBPR" e descubra uma nova maneira de comprar, manter e desfrutar do seu carro. Estamos ansiosos para atendê-lo!</p>
+
+            <?php
+include_once 'header.php';
+?>
+
+
+        <h2>Missão</h2>
+        <p>Nossa missão é proporcionar aos nossos clientes uma experiência de compra de veículos sem igual, oferecendo uma ampla gama de automóveis de alta qualidade e serviços excepcionais, sempre com integridade, transparência e foco no atendimento ao cliente.</p>
+
+        <h2>Visão</h2>
+        <p>Ser a referência no mercado automotivo, reconhecida pela excelência em atendimento, inovação e compromisso com a satisfação do cliente, contribuindo para um futuro mais sustentável e mobilidade inteligente.</p>
+
+        <h2>Valores</h2>
+        <ul>
+            <li><strong>Excelência no Atendimento:</strong> Priorizar a satisfação do cliente, garantindo uma experiência positiva em cada interação.</li>
+            <li><strong>Integridade e Transparência:</strong> Conduzir todos os negócios com honestidade, garantindo que todas as transações sejam claras e justas.</li>
+            <li><strong>Inovação:</strong> Estar sempre à frente das tendências do mercado automotivo, oferecendo os produtos e serviços mais modernos e eficientes.</li>
+            <li><strong>Qualidade:</strong> Comprometer-se com a qualidade dos produtos e serviços oferecidos, garantindo a confiança e segurança dos clientes.</li>
+            <li><strong>Sustentabilidade:</strong> Promover práticas sustentáveis e responsáveis que minimizem o impacto ambiental e contribuam para um futuro melhor.</li>
+            <li><strong>Respeito:</strong> Tratar todos os clientes, parceiros e colaboradores com respeito e dignidade.</li>
+            <li><strong>Compromisso:</strong> Dedicar-se a atender às necessidades dos clientes de maneira eficiente e responsável.</li>
+        </ul>
+    </section>
+</main>
+</body>
+</html>
+
+<?php
+include_once 'footer.php';
+?>
         </section>
     </main>
     
@@ -24,10 +54,5 @@ include_once 'header.php';
 </html>
 
 <?php
-
-
-
-
-include_once 'footer.php';
 
 ?>
